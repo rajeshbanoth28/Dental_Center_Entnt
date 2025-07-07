2c44697bc8a9b092220e6b9495bb1c1500d1fffe
rajeshbanoth28 <rajeshrajnayak2003@gmail.com>
2025-07-07 23:32:05 +0530

    Added Signup route and corrected logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,72 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import PatientView from "./components/PatientView";
-import { useAuth } from "./context/AuthContext";
 import CalendarView from "./components/CalendarView";
+import SignupPage from "./components/SignpPage";
+import { useAuth } from "./context/AuthContext";
+
 function App() {
   const { user } = useAuth();
-  if (!user) return <Login />;
+  const location = useLocation();
 
   return (
     <Routes>
-      {user.role === "Admin" && <Route path="/" element={<Dashboard />} />}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignupPage />} />
 
-      {user.role === "Patient" && (
-        <Route path="/patient" element={<PatientView />} />
-      )}
+      <Route
+        path="/"
+        element={
+          user ? (
+            user.role === "Admin" ? (
+              <Dashboard />
+            ) : (
+              <Navigate to="/patient" />
+            )
+          ) : (
+            <Navigate to="/login" state={{ from: location }} replace />
+          )
+        }
+      />
+
+      <Route
+        path="/patient"
+        element={
+          user ? (
+            user.role === "Patient" ? (
+              <PatientView />
+            ) : (
+              <Navigate to="/" />
+            )
+          ) : (
+            <Navigate to="/login" state={{ from: location }} replace />
+          )
+        }
+      />
+
+      <Route
+        path="/calendar"
+        element={
+          user ? (
+            <CalendarView />
+          ) : (
+            <Navigate to="/login" state={{ from: location }} replace />
+          )
+        }
+      />
+
       <Route
         path="*"
-        element={<Navigate to={user.role === "Admin" ? "/" : "/patient"} />}
+        element={
+          user ? (
+            <Navigate to={user.role === "Admin" ? "/" : "/patient"} />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
       />
-      <Route path="/calendar" element={<CalendarView />}/>
-      
     </Routes>
   );
 }
